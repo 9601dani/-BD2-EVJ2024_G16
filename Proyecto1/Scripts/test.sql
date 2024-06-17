ed4bda4b417236341cf5805d444b6f918f6d5b8c
Isaac03483 <levihernandez2501@gmail.com>
2024-06-17 10:36:43 -0600

    ADD DDL.sql and test.sql

---
+++ Proyecto1/Scripts/test.sql
@@ -0,0 +1,18 @@
+USE BD2
+GO
+
+SELECT * FROM proyecto1.Course
+SELECT * FROM proyecto1.CourseAssignment
+SELECT * FROM proyecto1.CourseTutor
+SELECT * FROM proyecto1.HistoryLog
+SELECT * FROM proyecto1.Notification
+SELECT * FROM proyecto1.ProfileStudent
+SELECT * FROM proyecto1.Roles
+SELECT * FROM proyecto1.TFA
+SELECT * FROM proyecto1.TutorProfile
+SELECT * FROM proyecto1.UsuarioRole
+SELECT * FROM proyecto1.Usuarios
+GO
+
+SELECT * FROM dbo.__EFMigrationsHistory
+GO
